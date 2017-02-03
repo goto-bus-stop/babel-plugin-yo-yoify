4ad39b361abe1533c0e9525a3f7e0368bdd1abc7
René Kooi <renee@kooi.me>
2017-02-03 16:22:14 +0100

    Add missing svg test files.

---
+++ test/fixtures/svg.expected.js
@@ -0,0 +1,10 @@
+var _path,
+    _svgNamespace = 'http://www.w3.org/2000/svg',
+    _svg,
+    _appendChild = require('yo-yoify/lib/appendChild'),
+    _svg2,
+    _div;
+
+const svg = (_svg = document.createElementNS(_svgNamespace, 'svg'), _svg.setAttribute('viewbox', '0 14 32 18'), _appendChild(_svg, ['\n    ', (_path = document.createElementNS(_svgNamespace, 'path'), _path.setAttribute('d', 'M2 14 V18 H6 V14z'), _path), '\n  ']), _svg);
+
+const htmlAndSvg = (_div = document.createElement('div'), _appendChild(_div, ['\n    ', (_svg2 = document.createElementNS(_svgNamespace, 'svg'), _svg2.textContent = '\n  ', _svg2), '\n']), _div);
