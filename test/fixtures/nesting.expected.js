f3fb195bf98775b30455339eb5f90b519eb455db
Renée Kooi <renee@kooi.me>
2017-04-05 12:30:49 +0200

    Strip unnecessary whitespace from tag contents, closes #6 (#12)

--- test/fixtures/nesting.expected.js
+++ test/fixtures/nesting.expected.js
@@ -2,4 +2,4 @@ var _b,
     _a,
     _appendChild = require('yo-yoify/lib/appendChild');
 
-_a = document.createElement('div'), _a.setAttribute('class', 'a'), _appendChild(_a, ['\n    ', (_b = document.createElement('div'), _b.setAttribute('class', 'b'), _b.textContent = '\n  ', _b), '\n']), _a;
+_a = document.createElement('div'), _a.setAttribute('class', 'a'), _appendChild(_a, [' ', (_b = document.createElement('div'), _b.setAttribute('class', 'b'), _b.textContent = ' ', _b), '\n']), _a;
